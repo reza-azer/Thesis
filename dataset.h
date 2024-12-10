07238e62a2e1e4ae740659b27326734e26a326bc
reza-azer <101710548+reza-azer@users.noreply.github.com>
2024-12-10 16:10:12 +0700

    Add files via upload

---
+++ dataset.h
@@ -0,0 +1,40 @@
+#ifndef dataset_H
+#define dataset_H
+#include <Arduino.h>
+
+class dataset{
+    public:
+        int baris,kolom;
+        byte wajah [40][70];
+        Senang1 ();
+        Senang2 ();
+        Senang3 ();
+        Senang4 ();
+        Senang5 ();
+        Senang6 ();
+        Senang7 ();
+        Senang8 ();
+        Senang9 ();
+        Senang10 ();
+        Netral1 ();
+        Netral2 ();
+        Netral3 ();
+        Netral4 ();
+        Netral5 ();
+        Netral6 ();
+        Netral7 ();
+        Netral8 ();
+        Netral9 ();
+        Netral10 ();
+        Antusias1 ();
+        Antusias2 ();
+        Antusias3 ();
+        Antusias4 ();
+        Antusias5 ();
+        Antusias6 ();
+        Antusias7 ();
+        Antusias8 ();
+        Antusias9 ();
+        Antusias10 ();
+};
+#endif
